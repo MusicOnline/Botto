4aa2969369eed21561b25a362ba36ef03ce2b7fc
MusicOnline <39178127+MusicOnline@users.noreply.github.com>
2019-03-09 08:33:03 +0800

    Add TRANSPARENT_DARK and comment to constants.py

--- botto/utils/constants.py
+++ botto/utils/constants.py
@@ -1,5 +1,9 @@
 from discord import PartialEmoji  # type: ignore
 
+# Feel free to add/change constants with caution for pre-exsting constants!
+
+TRANSPARENT_DARK = 0x36393E
+
 aLOADING = PartialEmoji(True, "loading", 420942895638642699)
 
 CHECK = PartialEmoji(False, "check", 474939098075758604)
