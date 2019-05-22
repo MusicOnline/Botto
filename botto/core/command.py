1ba1324b0a573f7313d4c50d8d10593701346083
MusicOnline <39178127+MusicOnline@users.noreply.github.com>
2019-05-22 17:25:02 +0800

    Fix custom group's superclasses and prepare help embed changes

--- botto/core/command.py
+++ botto/core/command.py
@@ -1,11 +1,36 @@
+import asyncio
 import functools
+import inspect
 
+import discord
+import yaml
 from discord.ext import commands  # type: ignore
 from discord.ext.commands.core import hooked_wrapped_callback  # type: ignore
 
 
 class Command(commands.Command):
-    pass
+    def help_embed(self, coro):
+        if not asyncio.iscoroutinefunction(coro):
+            raise TypeError("The error handler must be a coroutine.")
+
+        self._help_embed_func = coro
+        return coro
+
+    async def get_help_embed(self, helpcommand) -> discord.Embed:
+        if self.cog:
+            return await self._help_embed_func(self.cog, helpcommand)
+        return await self._help_embed_func(helpcommand)
+
+    @property
+    def short_doc(self) -> str:
+        if self.brief is not None:
+            return self.brief
+        if self.help is not None:
+            items = yaml.full_load(self.help)
+            if isinstance(items, dict):
+                return items.get("short", "Information not available.")
+            return self.help.split("\n", 1)[0]
+        return "Information not available."
 
 
 class GroupMixin(commands.GroupMixin):
@@ -28,80 +53,8 @@ class GroupMixin(commands.GroupMixin):
         return decorator
 
 
-class Group(GroupMixin, Command):
-    def __init__(self, *args, **attrs):
-        self.invoke_without_command = attrs.pop("invoke_without_command", False)
-        super().__init__(*args, **attrs)
-
-    def copy(self):
-        ret = super().copy()
-        for cmd in self.commands:
-            ret.add_command(cmd.copy())
-        return ret
-
-    async def invoke(self, ctx):
-        early_invoke = not self.invoke_without_command
-        if early_invoke:
-            await self.prepare(ctx)
-
-        view = ctx.view
-        previous = view.index
-        view.skip_ws()
-        trigger = view.get_word()
-
-        if trigger:
-            ctx.subcommand_passed = trigger
-            ctx.invoked_subcommand = self.all_commands.get(trigger, None)
-
-        if early_invoke:
-            injected = hooked_wrapped_callback(self, ctx, self.callback)
-            await injected(*ctx.args, **ctx.kwargs)
-
-        if trigger and ctx.invoked_subcommand:
-            ctx.invoked_with = trigger
-            await ctx.invoked_subcommand.invoke(ctx)
-        elif not early_invoke:
-            # undo the trigger parsing
-            view.index = previous
-            view.previous = previous
-            await super().invoke(ctx)
-
-    async def reinvoke(self, ctx, *, call_hooks=False):
-        early_invoke = not self.invoke_without_command
-        if early_invoke:
-            ctx.command = self
-            await self._parse_arguments(ctx)
-
-            if call_hooks:
-                await self.call_before_hooks(ctx)
-
-        view = ctx.view
-        previous = view.index
-        view.skip_ws()
-        trigger = view.get_word()
-
-        if trigger:
-            ctx.subcommand_passed = trigger
-            ctx.invoked_subcommand = self.all_commands.get(trigger, None)
-
-        if early_invoke:
-            try:
-                await self.callback(*ctx.args, **ctx.kwargs)
-            except:
-                ctx.command_failed = True
-                raise
-            finally:
-                if call_hooks:
-                    await self.call_after_hooks(ctx)
-
-        if trigger and ctx.invoked_subcommand:
-            ctx.invoked_with = trigger
-            await ctx.invoked_subcommand.reinvoke(ctx, call_hooks=call_hooks)
-        elif not early_invoke:
-            # undo the trigger parsing
-            view.index = previous
-            view.previous = previous
-            await super().reinvoke(ctx, call_hooks=call_hooks)
+class Group(GroupMixin, Command, commands.Group):
+    pass
 
 
 command = functools.partial(commands.command, cls=Command)
