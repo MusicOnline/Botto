5db5d30688b2a73eeae577695c43e1c734e840f0
MusicOnline <39178127+MusicOnline@users.noreply.github.com>
2020-01-02 02:01:46 +0800

    Use uvloop if possible

--- botto/core/bot.py
+++ botto/core/bot.py
@@ -19,6 +19,13 @@ try:
 except ImportError:
     import json  # type: ignore
 
+try:
+    import uvloop
+except ImportError:
+    pass
+else:
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
 logger = logging.getLogger(__name__)
 
 
